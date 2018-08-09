a8708087c54a49f29b26b96c59af1e00df058c01
Andrew Catton <>
2018-08-09 16:05:36 +0000

    Stop unnecessarily wrapping the google client API

--- actions/add-item/function.js
+++ actions/add-item/function.js
@@ -1,17 +1,30 @@
 function(itemText, ellipsis) {
-  const gsheet = require('gsheet')(ellipsis);
+  const client = require('google-client')(ellipsis);
+const {google} = require('googleapis');
+const sheets = google.sheets('v4');
 
 const timestamp = (new Date()).toString();
 const slackHandle = ellipsis.userInfo.messageInfo.details.profile.displayName;
-
-gsheet.addRows(ellipsis.env.CEO_BRIEFING_SHEET_ID, [
+const values = [
   [timestamp, slackHandle, itemText]
-]).then(res => {
-  const updated = res.data.updates.updatedRows;
-  if (updated == 0) {
-    ellipsis.error("Hmmm… I couldn't add your item for some reason.");
-  } else {
-    ellipsis.success(itemText);
-  }
+];
+client.authorize().then(() => {
+  const request = {
+    spreadsheetId: ellipsis.env.CEO_BRIEFING_SHEET_ID,
+    range: 'Form Responses 1',
+    valueInputOption: 'USER_ENTERED',
+    resource: {
+      values: values
+    },
+    auth: client,
+  };
+  return sheets.spreadsheets.values.append(request).then(res => {
+    const updated = res.data.updates.updatedRows;
+    if (updated == 0) {
+      ellipsis.error("Hmmm… I couldn't add your item for some reason.");
+    } else {
+      ellipsis.success(itemText);
+    }
+  });        
 });
 }
--- actions/show-items/function.js
+++ actions/show-items/function.js
@@ -1,14 +1,23 @@
 function(ellipsis) {
-  const gsheet = require('gsheet')(ellipsis);
+  const client = require('google-client')(ellipsis);
+const {google} = require('googleapis');
+const sheets = google.sheets('v4');
 
-gsheet.getAllRows(ellipsis.env.CEO_BRIEFING_SHEET_ID).then(res => {
-  const values = res.data.valueRanges[0].values.slice(1).map(ea => {
-    return {
-      timestamp: ea[0],
-      user: ea[1],
-      text: ea[2]
-    };
+client.authorize().then(() => {
+  const request = {
+    spreadsheetId: ellipsis.env.CEO_BRIEFING_SHEET_ID,
+    ranges: ['Form Responses 1'],
+    auth: client,
+  };
+  sheets.spreadsheets.values.batchGet(request).then(res => {
+    const values = res.data.valueRanges[0].values.slice(1).map(ea => {
+      return {
+        timestamp: ea[0],
+        user: ea[1],
+        text: ea[2]
+      };
+    });
+    ellipsis.success(values);
   });
-  ellipsis.success(values);
 });
 }
